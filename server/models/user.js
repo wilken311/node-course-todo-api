657d602208f46d0bd01a254e43c4c73ea9cb4dd6
wilken montero <monterowilken311@gmail.com>
2019-03-30 13:14:02 +0800

    ADD GET/users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,83 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
-    email: {
-      type: String,
-      required: true,
-      trim: true,
-      minlength: 1
+var UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
     }
-  });
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
+});
 
-  module.exports={User};
+UserSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+  // user.tokens.push({access, token});
+  user.tokens=user.tokens.concat([{access,token}]);
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+
+/*.statics is an object like methods although everything you odd onto it turns into a model method as opposed
+* to and instance method
+*/
+
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    // return new Promise((resolve,reject)=>{
+    //   reject();
+    // });
+    return Promise.reject(); 
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports = {User}
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
@@ -5,12 +6,17 @@ var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
+
+
 
 var app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+
+//POST
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
@@ -23,6 +29,8 @@ app.post('/todos', (req, res) => {
   });
 });
 
+
+//GET
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
@@ -31,6 +39,9 @@ app.get('/todos', (req, res) => {
   });
 });
 
+
+
+//GET:ID
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
@@ -49,6 +60,8 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
+
+//DELETE
 app.delete('/todos/:id', (req, res) => {
   var id = req.params.id;
 
@@ -66,6 +79,55 @@ app.delete('/todos/:id', (req, res) => {
     res.status(400).send();
   });
 });
+
+
+//PATCH
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+});
+
+
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
+
+app.get('/users/me',authenticate,(req, res) => {
+  res.send(req.user);
+});
+
 
 
 app.listen(port, () => {
